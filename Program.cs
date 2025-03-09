15e67f7e6b33298723270d06dd619197c2678f66
Антон <antonnechlopochin@mail.ru>
2025-03-09 16:52:15 +0300

    feat: added a file copy feature

--- Program.cs
+++ Program.cs
@@ -41,6 +41,33 @@ class Program
             Console.WriteLine($"{colFiles}. {i} \n");
         }
     }
+    public static void CopyFiles(string request){ //метод для копирования файла или папки
+        try{
+            Console.Write("\n Введите название файла или папки которое вы хотите скопировать: ");
+            string? fileName = Console.ReadLine();
+            if(fileName != null && (Directory.Exists(request + @"\" + fileName) || File.Exists(request + @"\" + fileName))){ //проверка существует ли файл или папка
+                FileInfo oldFile = new FileInfo(request + @"\" + fileName); //с помощью объекта буду копировать папку
+                Console.Write("\n Введите абсолютный путь, куда вы хотите скопировать: ");
+                string? newFileplace = Console.ReadLine();
+                if(newFileplace != null && Directory.Exists(newFileplace)){ //проверка существует ли такой путь
+                    /*if(Directory.Exists(request + @"\" + fileName)){
+                        Console.WriteLine("Хотите ли вы скопировать содержимое папки?(true/false)");
+                        string? InDirectory = Console.ReadLine().ToLower();
+                        if(InDirectory != null && (InDirectory == "true" || InDirectory == "false")){
+                            oldFile.CopyTo(newFileplace + @"\" + fileName, Boolean.Parse(InDirectory));
+                            Console.WriteLine("Папка успешно скопирована");
+                        }
+                    }*/
+                    if(File.Exists(request + @"\" + fileName)){
+                        File.Copy(request + @"\" + fileName, newFileplace + @"\" + fileName);
+                    }
+                }
+            }
+        }
+        catch(Exception ex){
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
  
 
