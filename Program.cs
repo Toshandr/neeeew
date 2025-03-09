7a7afd939aa7c5983935983cbd4621bd82e36b28
Антон <antonnechlopochin@mail.ru>
2025-03-09 19:54:25 +0300

    feat: add function to copy folders

--- Program.cs
+++ Program.cs
@@ -41,25 +41,43 @@ class Program
             Console.WriteLine($"{colFiles}. {i} \n");
         }
     }
+
+    public static void recursionFolder(string sourceDir, string targetDir){ //метод для переноса всего католга целиком
+        Directory.CreateDirectory(targetDir); //создаём в папке назаначения новую папку
+
+        
+        foreach (string filePath in Directory.GetFiles(sourceDir)) //из начальной папки берём все файлы
+        {
+            string fileName = Path.GetFileName(filePath); //берём имя файла
+            string destFilePath = Path.Combine(targetDir, fileName); //прессуем имя файла и путь на новую папку - получаем путь до нового файла
+            File.Copy(filePath, destFilePath, true); //переносим файлы в новую папку
+        }
+
+        
+        foreach (string dirPath in Directory.GetDirectories(sourceDir)) //берём все подкаталоги
+        {
+            string dirName = Path.GetFileName(dirPath); //берём имя каждой папки
+            string destSubDir = Path.Combine(targetDir, dirName); //прессуем и получаем ссылку на новый подкаталог в целевом каталоге
+            recursionFolder(dirPath, destSubDir); //повтороно вызываем метод но уже проходимся по подкаталогам
+        }
+    }
     public static void CopyFiles(string request){ //метод для копирования файла или папки
         try{
             Console.Write("\n Введите название файла или папки которое вы хотите скопировать: ");
             string? fileName = Console.ReadLine();
             if(fileName != null && (Directory.Exists(request + @"\" + fileName) || File.Exists(request + @"\" + fileName))){ //проверка существует ли файл или папка
-                FileInfo oldFile = new FileInfo(request + @"\" + fileName); //с помощью объекта буду копировать папку
                 Console.Write("\n Введите абсолютный путь, куда вы хотите скопировать: ");
                 string? newFileplace = Console.ReadLine();
                 if(newFileplace != null && Directory.Exists(newFileplace)){ //проверка существует ли такой путь
-                    /*if(Directory.Exists(request + @"\" + fileName)){
-                        Console.WriteLine("Хотите ли вы скопировать содержимое папки?(true/false)");
-                        string? InDirectory = Console.ReadLine().ToLower();
-                        if(InDirectory != null && (InDirectory == "true" || InDirectory == "false")){
-                            oldFile.CopyTo(newFileplace + @"\" + fileName, Boolean.Parse(InDirectory));
-                            Console.WriteLine("Папка успешно скопирована");
-                        }
-                    }*/
-                    if(File.Exists(request + @"\" + fileName)){
+                    if(Directory.Exists(request + @"\" + fileName)){ // копирование если объект - папка
+                        string newfullfolderpath = newFileplace + @"\" + fileName;
+                        string oldfullfolderpath = request + @"\" + fileName;
+                        recursionFolder(oldfullfolderpath, newfullfolderpath);
+                        Console.WriteLine("Копирование выполнено успешно");
+                    }
+                    if(File.Exists(request + @"\" + fileName)){ // копирование если объект - файл
                         File.Copy(request + @"\" + fileName, newFileplace + @"\" + fileName);
+                        Console.WriteLine("Копирование выполнено успешно");
                     }
                 }
             }
