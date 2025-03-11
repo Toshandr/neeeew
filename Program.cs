93c04d6e994c3694545fe5c9e303a057cabebb52
Антон <antonnechlopochin@mail.ru>
2025-03-11 21:50:03 +0300

    feat:added a function for displaying information about files

--- Program.cs
+++ Program.cs
@@ -4,26 +4,30 @@
 namespace neeeew;
 
 
-class Person
-{
-    public string Name { get;}
-    public int Age { get; set; }
-    public Person(string name, int age)
-    {
-        Name = name;
-        Age = age;
-    }
-}
 
 class Program
 {
+
+    /*программа работает по такому принципу:
+    1.Пользователь выбирает папку старта, откуда он начнёт работу
+    2.Пользователь видит все файлы и папки каталога
+    3.Выбирает команду(которые пока описаны как методы)
+    4.Выбирает файл, не прописывая полный путь до него, то есть пользователь может узнать инфу о файлу только когда перейдёт в его родительский каталог*/
+
     static void Main(string[] args)
     {
         Console.Write("Введите абсолютный путь до папки с которой вы хотите начать работать: ");
         string? request = Console.ReadLine();
         if(request != null && Directory.Exists(request)){
-            ReadAll(request);
-            CopyFiles(request);
+
+
+            // Проба методов, чтобы проверить работу одного из них раскомментируйте нужный
+
+
+            //ReadAll(request); 
+            //CopyFiles(request);
+            FileInformation(request);
+            //DeleteFiles(request);
         }
     }
     
@@ -42,7 +46,7 @@ class Program
         }
     }
 
-    public static void recursionFolder(string sourceDir, string targetDir){ //метод для переноса всего католга целиком
+    public static void recursionFolder(string sourceDir, string targetDir){ //метод для переноса католга целиком
         Directory.CreateDirectory(targetDir); //создаём в папке назаначения новую папку
 
         
@@ -63,13 +67,13 @@ class Program
     }
     public static void CopyFiles(string request){ //метод для копирования файла или папки
         try{
-            Console.Write("\n Введите название файла или папки которое вы хотите скопировать: ");
+            Console.Write("\nВведите название файла или папки которые вы хотите скопировать: ");
             string? fileName = Console.ReadLine();
             if(fileName != null && (Directory.Exists(Path.Combine(request, fileName)) || File.Exists(Path.Combine(request, fileName)))){ //проверка существует ли файл или папка
-                Console.Write("\n Введите абсолютный путь, куда вы хотите скопировать: ");
+                Console.Write("\nВведите абсолютный путь, куда вы хотите скопировать: ");
                 string? newFileplace = Console.ReadLine();
                 if(newFileplace != null && Directory.Exists(newFileplace)){ //проверка существует ли такой путь
-                    if(Directory.Exists(request + @"\" + fileName)){ // копирование если объект - папка
+                    if(Directory.Exists(Path.Combine(request, fileName))){ // копирование если объект - папка
                         string newfullfolderpath = Path.Combine(newFileplace, fileName);
                         string oldfullfolderpath = Path.Combine(request, fileName);
                         recursionFolder(oldfullfolderpath, newfullfolderpath);
@@ -80,12 +84,46 @@ class Program
                         Console.WriteLine("Копирование выполнено успешно");
                     }
                 }
+                else{
+                    Console.WriteLine("Такого каталога нет");
+                }
+            }
+            else{
+                Console.WriteLine("Такого файла нет");
             }
         }
         catch(Exception ex){
             Console.WriteLine(ex.Message);
         }
     }
+    public static void FileInformation(string request){//метод для вывода основной информации
+        try{
+            Console.Write("\nВведите название файла о котором вы хотите узнать: ");
+            string? fileName = Console.ReadLine();
+            if(fileName != null && (File.Exists(Path.Combine(request, fileName)) || Directory.Exists(Path.Combine(request, fileName)))){ //проверка есть ли файл или папка с таким названием
+                if(File.Exists(Path.Combine(request, fileName))){// проверяем существует ли такой файл
+                    FileInfo file = new FileInfo(Path.Combine(request, fileName));
+                    Console.WriteLine("Размер файла: " + file.Length / 1024.0  + "КБ");
+                    Console.WriteLine("Расширение файла: " + file.Extension);
+                    Console.WriteLine("Абсолютный путь: " + file.FullName);
+                }
+                if(Directory.Exists(Path.Combine(request, fileName))){ //проверяем существует ли такая папка
+                    DirectoryInfo dir = new DirectoryInfo(Path.Combine(request, fileName));
+                    Console.WriteLine("Размер папки: " + dir.EnumerateFiles("", SearchOption.AllDirectories).Sum(fi => fi.Length)/1024 + "КБ"); //находим все файлы в каталоге и берём у каждого файла его размер
+                    Console.WriteLine("Расширение: каталог");
+                    Console.WriteLine("Абсолютный путь: " + dir.FullName);
+                }
+            }
+            else{
+                Console.WriteLine("Такого файла нет.");
+            }
+        }
+        catch(Exception ex){
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+
 }
  
 
